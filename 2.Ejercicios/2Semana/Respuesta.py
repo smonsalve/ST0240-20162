a25677990bb2f36e1b9cb18b6fe7ce6fe6afc885
Sergio Monsalve <smonsalve@gmail.com>
2016-07-26 15:43:51 -0500

    update

---
+++ 2.Ejercicios/2Semana/Respuesta.py
@@ -0,0 +1,13 @@
+nombre = raw_input()
+
+apellido1 = "Silva"
+apellido2 = "Pineda"
+apellido3 = "Monsalve"
+
+codigo1 = 200120039010
+codigo2 = 199810024010
+codigo3 = 200410061010
+
+print nombre + " " + apellido1 + " " + str(codigo1)
+print nombre + " " + apellido2 + " " + str(codigo2)
+print nombre + " " + apellido3 + " " + str(codigo3)
