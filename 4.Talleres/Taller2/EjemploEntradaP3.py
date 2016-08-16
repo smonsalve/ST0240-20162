8429245f3058dbdb3623cc53fb0b610838b06433
Sergio Monsalve <smonsalve@gmail.com>
2016-08-16 04:29:56 -0500

    ejemplo entrada tortugas

---
+++ 4.Talleres/Taller2/EjemploEntradaP3.py
@@ -0,0 +1,20 @@
+t = int(raw_input())
+
+def zanb(lista):
+    impo = 0
+    ### aca es donde ustedes escriben su algoritmo que calcula las tortugas importadas
+    return impo
+
+for i in range(t):
+
+    # a = [int(x) for x in raw_input().split()]     #Esta linea hace lo mismo que las siguientes lineas
+    
+    a = raw_input()
+    b = a.split()
+    c = []
+    for item in b:
+        d = int(item)
+        c.append(d)
+    #print c
+        
+    print zanb(c)
